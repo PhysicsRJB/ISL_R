cbfb6a33859f7fe3ecd2b22a664eb940c83639bb
PhysicsRJB <robertjanbuda@gmail.com>
2019-10-03 11:38:52 -0400

    Solve exercise 9

--- Chapter_2.R
+++ Chapter_2.R
@@ -1,3 +1,4 @@
+#Exercise 8
 college = read.csv("College.csv", header = T)
 fix(college)
 rownames(college) = college[ , 1]
@@ -21,4 +22,23 @@ hist(Books, col = 3, breaks = 30)
 hist(perc.alumni, col = 4, breaks = 30)
 hist(S.F.Ratio, col = 5, breaks = 30)
 
+#Exercise 9
+Auto = read.csv("Auto.csv", header = T, na.strings = "?")
+fix(Auto)
+Auto = na.omit(Auto)
+length(colnames(Auto))
+for (i in seq(1,length(colnames(Auto)))){
+  print(range(Auto[, i]))
+  print(colnames(Auto)[i])
+  }
+
+summary(Auto)
+
+summary(Auto[ -c(10:85), ])
+
+pairs(Auto[ , -9])
+
+cor(Auto[ , -9])
+#to predict mpg, I'd use displacement, horsepower, displacement and weight, as they seem very corellated.
+#Also, cylinders might be useful. These predictors show highest correlation
 
